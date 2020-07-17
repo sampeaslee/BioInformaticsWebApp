eef62be158f111c3bbb86109fbf2bf637c17f4d7
Hgeng <hgeng7@wisc.edu>
2020-07-17 02:44:24 -0500

    NEW VERSION 717

--- src/main/java/CS_564/Metabolites/StoichiometryRepo.java
+++ src/main/java/CS_564/Metabolites/StoichiometryRepo.java
@@ -8,10 +8,13 @@ import org.springframework.data.jpa.repository.Query;
 public interface StoichiometryRepo extends JpaRepository<Stoichiometry, Integer> {
     
     
-    @Query(value = " select * from stoichometry limit 1000 ;", nativeQuery = true)
+    @Query(value = " select * from stoichiometry limit 1000 ;", nativeQuery = true)
     public ArrayList<Stoichiometry> getStoichiometry();
 
-    @Query(value = "SELECT * FROM stoichometry s WHERE s.startMetabolites LIKE %:Metaboliteid% or  s.endMetabolites LIKE %:Metaboliteid%",  nativeQuery = true)
+    @Query(value = "SELECT * FROM stoichiometry s WHERE s.reactionid = :reactionid",  nativeQuery = true)
+    public Stoichiometry findByReactionID(@Param("reactionid") String reactionid);
+
+    @Query(value = "SELECT * FROM stoichiometry s WHERE s.startMetabolites LIKE %:Metaboliteid% or  s.endMetabolites LIKE %:Metaboliteid%",  nativeQuery = true)
     public ArrayList<Stoichiometry> autoSearch(@Param("Metaboliteid") String Metaboliteid);
     @Query(value = " select * from stoichometry ;", nativeQuery = true)
     public ArrayList<Stoichiometry> queryExample();
